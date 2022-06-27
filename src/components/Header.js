7cce1e1b985fc1bfda6adf8db8cec89635062d12
Carlos Londono <c.londono.m@gmail.com>
2022-06-27 09:13:35 -0500

    editting readme

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,25 @@ const Header = () => {
     <>
       <div className='container flex md:mx-auto mt-1  justify-around '>
         <img src={dog} alt='dog.svg' width='60px' />
-
-        <h1 className='font-black text-5xl text-center '>
-          Patient Follow-up <span className='text-green-600'>Vet</span>
-        </h1>
+        <div className='text-center'>
+          <h1 className='font-black text-5xl text-center '>
+            Patient Follow-up <span className='text-green-600 '>Vet</span>
+          </h1>
+          <br />
+          <div>
+            <a
+              className='text-lg'
+              href='https://carlos-londono.dev/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              by{' '}
+              <span className='text-green-600 font-bold'>
+                carlos-londono.dev
+              </span>{' '}
+            </a>
+          </div>
+        </div>
 
         <img src={cat} alt='cat.svg' width='60px' />
       </div>
